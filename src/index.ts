24393b9e7d40e7283439936ec7e51a3d6440fc41
Oliver Smit <16165237+olmesm@users.noreply.github.com>
2023-09-30 12:58:42 +0100

    rm bun hell

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 const data = fs.readFileSync(0, "utf-8");
 
 const main = async () => {
-  console.log("Hello via Bun!", JSON.parse(data));
+  console.log(JSON.parse(data));
 };
 
 main();
